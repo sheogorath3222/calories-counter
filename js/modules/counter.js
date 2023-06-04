471d313198ac2f9e1727bf31e75afab8bb8388d3
Sheogorath <sheogorathlord@yandex.ru>
2023-06-04 13:44:00 +0500

    constants and ratios added

---
+++ js/modules/counter.js
@@ -0,0 +1,23 @@
+const CaloriesFormulaFactor = {
+  WEIGHT: 10,
+  HEIGHT: 6.25,
+  AGE: 5,
+};
+
+const CaloriesFormulaConstant = {
+  MALE: 5,
+  FEMALE: -161
+};
+
+const PhysicalActivityRatio = {
+  MIN: 1.2,
+  LOW: 1.375,
+  MEDIUM: 1.55,
+  HIGH: 1.725,
+  MAX: 1.9,
+};
+
+const CaloriesMinMaxRatio = {
+  MIN: 0.85,
+  MAX: 1.15
+};
